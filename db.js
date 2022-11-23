c4082ed7dae9adb5127b551abc7c5547b23b8f6e
saugat-28 <55578635+saugat-28@users.noreply.github.com>
2022-11-23 16:42:30 +0530

    Changed mongoURI to MongoDB Atlas URL

--- db.js
+++ db.js
@@ -1,11 +1,13 @@
 const mongoose = require('mongoose');
-const mongoURI = "mongodb://localhost:27017/scribble?directConnection=true&readPreference=primary"
+// const LOCAL_URI = "mongodb://localhost:27017/scribble?directConnection=true&readPreference=primary";
+const ATLAS_URI = "mongodb+srv://saugat:saugat28@cluster0.0my9t1x.mongodb.net/scribble?retryWrites=true&w=majority"
+const mongoURI = ATLAS_URI;
 
 // Connect To Mongo function connects backend to the database and display success message on successful connection
 const connectToMongo = () => {
     mongoose.connect(mongoURI, ()=>{
         console.log("Connected To MongoDB Successfully!\n")
     })
-}
+};
 
 module.exports = connectToMongo;
