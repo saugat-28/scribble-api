5390e643ef8369fbd2edf53858529b6418183aac
saugat-28 <55578635+saugat-28@users.noreply.github.com>
2022-11-24 22:37:22 +0530

    Added .env file

--- db.js
+++ db.js
@@ -1,7 +1,8 @@
+const dotenv = require('dotenv');
 const mongoose = require('mongoose');
-// const LOCAL_URI = "mongodb://localhost:27017/scribble?directConnection=true&readPreference=primary";
-const ATLAS_URI = "mongodb+srv://saugat:saugat28@cluster0.0my9t1x.mongodb.net/scribble?retryWrites=true&w=majority"
-const mongoURI = ATLAS_URI;
+dotenv.config();
+
+const mongoURI = process.env.ATLAS_URI;
 
 // Connect To Mongo function connects backend to the database and display success message on successful connection
 const connectToMongo = () => {
