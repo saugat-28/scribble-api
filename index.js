4e88fe81ac493fbe218def0afd8a02d85d2e4d9e
saugat-28 <55578635+saugat-28@users.noreply.github.com>
2022-11-24 23:40:33 +0530

    fixed a bug

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const connectToMongo = require('./db')
 const express = require('express')
 var cors = require('cors')
-const Note = require('./models/Note')
 
 // Connect to database
 connectToMongo();
@@ -22,19 +21,6 @@ app.use('/api/notes', require('./routes/notes'))
 // Display the following on Default localhost page on server Port
 app.get('/', (req, res) => {
   res.send('---Welcome To The Server Page of Scribble---')
-})
-
-// TEST ROUTE: Get All the Notes using: GET "/fetch". Login Required
-app.get('/fetch', fetchuser, async (req, res) => {
-  try {
-      // Fetch all notes that have user ID matching current user and return
-      const notes = await Note.find();
-      res.json(notes)
-  } catch (error) {
-      // In case of some error log the error and return Error Message
-      console.log(error.message)
-      res.status(500).send("Some Error Occured!")
-  }
 })
 
 // Start Server to Listen on Localhost's Port
